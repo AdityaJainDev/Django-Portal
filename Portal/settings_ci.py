85403783ad49ad20e247859af399dd16aea234c8
Aditya Jain <a.jain@aditsystems.de>
2022-03-04 17:06:54 +0100

    CI/CD test

--- Portal/settings_ci.py
+++ Portal/settings_ci.py
@@ -5,8 +5,7 @@ Django settings for sepaPayment project. for CI
 # pylint: disable=wildcard-import
 # pylint: disable=unused-wildcard-import
 
-from sepaPayment.settings import *
+from Portal.settings import *
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '::1']
 SECRET_KEY = "CI ONLY"
-
