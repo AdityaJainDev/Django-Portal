d57ee9a19765169b632b8de366b9d672735fd0ba
Aditya Jain <a.jain@aditsystems.de>
2022-03-04 16:15:27 +0100

    Form Changes and GitLab CI/CD

--- detailsPage/forms.py
+++ detailsPage/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from localflavor.generic.forms import BICFormField, IBANFormField 
 from django.utils.translation import gettext_lazy as _
+from django.forms.widgets import Widget, CheckboxInput, boolean_check
 
 PAYMENT_CHOICES =(
     ("0", _("Bank Transfer")),
@@ -9,10 +10,11 @@ PAYMENT_CHOICES =(
 
 class PaymentForm(forms.Form):
     account_number = forms.CharField(label=_('AccountNumber'), max_length = 100, required = False)
-    options = forms.ChoiceField(label=_('options'), choices = PAYMENT_CHOICES)
+    options = forms.MultipleChoiceField(label=_('options'), choices = PAYMENT_CHOICES, widget=forms.CheckboxSelectMultiple())
     owner = forms.CharField(label=_('owner'), max_length = 100, required = False)
     iban = IBANFormField(label= "IBAN", required = False)
     bic = BICFormField(label= "BIC", required = False)
+    confirm = forms.BooleanField(label= _('Accept'), required=True)
 
     def __init__(self, *args, **kwargs):
         super(PaymentForm, self).__init__(*args, **kwargs)
--- detailsPage/views.py
+++ detailsPage/views.py
@@ -9,13 +9,22 @@ from django.contrib import messages
 def index(request):
     return render(request, "templates/base.html")
 
-
 def sepa_payment(request):
 
+    crm_endpoint = "https://ascrm.aditsystems.de/api/Kunden/SEPA/"
+
     if request.method == 'GET':
-        form = PaymentForm()
-        form.initial['account_number'] = request.GET.get('knr', None)
-        form.initial['options'] = "1"
+        account_number = request.GET.get('knr', None)
+        token = request.GET.get('token', None)
+        data = {'knr':account_number, 'token':token}
+        save_data = requests.get(crm_endpoint, params=data).json()
+
+        if save_data['status'] == -1:
+            return HttpResponse(_('Token Message'))
+        else:
+            form = PaymentForm()
+            form.initial['account_number'] = request.GET.get('knr', None)
+            form.initial['options'] = "1"
 
     elif request.method == 'POST':
         form = PaymentForm(request.POST)
@@ -26,20 +35,31 @@ def sepa_payment(request):
             bic = form.cleaned_data['bic']
             options = form.cleaned_data['options']
             token = request.GET.get('token', None)
-            
-            data = {"inhaber": owner, "iban": iban, "bic": bic, 'knr':account_number, 'token':token, 'zahlungsart':options}
 
-            crm_endpoint = "https://ascrm.aditsystems.de/api/Kunden/SEPA"
-
-            save_data = requests.post(crm_endpoint, data)
-            
-            if save_data.json()['status'] == -1:
-                messages.error(request, _('Error Message'))
+            if len(options) > 1:
+                messages.error(request, _('Option Message'))
                 form = PaymentForm()
-                return HttpResponseRedirect(request.META.get('HTTP_REFERER'), {'form':form})
+                form.initial['account_number'] = request.GET.get('knr', None)
+                form.initial['options'] = "1"
+
             else:
-                messages.success(request, _('Success Message'))
-                return HttpResponseRedirect("/")
+                data = {"inhaber": owner, "iban": iban, "bic": bic, 'knr':account_number, 'token':token, 'zahlungsart':options}
+
+                save_data = requests.post(crm_endpoint, data)
+                
+                if save_data.json()['status'] == -1:
+                    messages.error(request, _('Error Message'))
+                    form = PaymentForm()
+                    form.initial['account_number'] = request.GET.get('knr', None)
+                    form.initial['options'] = "1"
+                    return HttpResponseRedirect(request.META.get('HTTP_REFERER'), {'form':form})
+                else:
+                    return HttpResponse(_('Success Message'))
+        else:
+            messages.error(request, _('Error Message'))
+            form = PaymentForm()
+            form.initial['account_number'] = request.GET.get('knr', None)
+            form.initial['options'] = "1"
 
     context = {'form':form}
 
