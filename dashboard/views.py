f707b65405f9f35b918da7a78f69218e91ca5598
Aditya Jain <a.jain@aditsystems.de>
2022-03-10 11:57:30 +0100

    Small Fix

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_GET
 # Create your views here.
 
-@require_POST
+@require_GET
 def index(request):
     return render(request, "home.html")
